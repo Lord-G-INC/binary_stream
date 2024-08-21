d4bf488372cf64a9df796f84fbf226d07480455d
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2024-08-21 08:54:14 -0500

    Add Util.cs

---
+++ Util.cs
@@ -0,0 +1,19 @@
+﻿namespace binary_stream;
+
+public static class Util
+{
+    public static T FromBytes<T>(ReadOnlySpan<byte> bytes, Endian endian, Encoding? enc = null) 
+        where T : IRead, new()
+    {
+        using BinaryStream stream = new(bytes) { Endian = endian, Encoding = enc ?? Encoding.UTF8 };
+        return stream.ReadItem<T>();
+    }
+
+    public static byte[] IntoBytes<T>(this T item, Endian endian, Encoding? enc = null)
+        where T : IWrite
+    {
+        using BinaryStream stream = new() { Endian = endian, Encoding = enc ?? Encoding.UTF8 };
+        stream.WriteItem(item);
+        return stream.ToArray();
+    }
+}
