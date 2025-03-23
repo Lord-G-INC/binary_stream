03bce8bfeecc6d154f45e2a9f0a73d2639f6bad5
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2025-03-23 13:37:55 -0500

    Add new interfaces

--- Interfaces.cs
+++ Interfaces.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Binary_Stream;
 
 public interface IRead {
@@ -6,4 +8,25 @@ public interface IRead {
 
 public interface IWrite {
     public void Write(BinaryStream stream);
+}
+
+public interface ILoadable<T> where T : ILoadable<T> {
+    public abstract static T LoadFrom(BinaryStream stream, Endian? endian = null, Encoding? enc = null);
+}
+
+public interface ILoadTo {
+    public T LoadTo<T>(Endian? endian = null, Encoding? enc = null) where T : ILoadable<T>;
+}
+
+public class Loader<T>(T item) : ILoadable<Loader<T>> where T : IRead, new() {
+    readonly T item = item;
+
+    public static Loader<T> LoadFrom(BinaryStream stream, Endian? endian = null, Encoding? enc = null)
+    {
+        stream.Endian = endian ?? stream.Endian;
+        stream.Encoding = enc ?? stream.Encoding;
+        return new(stream.ReadItem<T>());
+    }
+    public static implicit operator T(Loader<T> self) => self.item;
+    public static implicit operator Loader<T>(T self) => new(self);
 }
